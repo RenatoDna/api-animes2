94720417535c732b3cd0a5e5cee768c2e6acb461
RenatoDna <dna.renato@gmail.com>
2025-11-10 17:54:30 -0300

    fix(renato): adição aquivo jest.config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: "node",
+  transform: {},
+  extensionsToTreatAsEsm: [".js"],
+  moduleFileExtensions: ["js", "json"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+};
